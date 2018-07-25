eb03b554301add447c36a550598dac816439a1b1
李桂治 <ligz@healthmall.cn>
2018-07-25 11:18:57 +0800

    添加tableview

--- SwitDemo/SwitDemo/Modules/HomeModule(首页)/DRHomeViewController.swift
+++ SwitDemo/SwitDemo/Modules/HomeModule(首页)/DRHomeViewController.swift
@@ -8,13 +8,74 @@
 
 import UIKit
 
-class DRHomeViewController: DRViewController {
+class DRHomeViewController: DRViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    var tableView:UITableView!
+    var dataArray:[String]!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        initSubView()
     }
+    
+    func initSubView() {
+        
+        self.dataArray = [String]()
+        for i in 1...20
+        {
+            self.dataArray.append("第\(i)行")
+        }
+        
+        
+        self.tableView = UITableView(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height), style: UITableViewStyle.plain)
+        self.tableView.delegate = self;
+        self.tableView.dataSource = self;
+        self.view.addSubview(self.tableView)
+        
+    }
+    //UITableViewDelegate,UITableViewDataSource
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataArray.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellID = "cellID"
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        
+        if cell == nil
+        {
+            cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: cellID)
+        }
+        
+        cell!.textLabel?.text = self.dataArray![indexPath.row]
+        
+        return cell!
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        let alertController = UIAlertController(title: "提示", message: "测试", preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "cancel", style: .cancel, handler: nil)
+        let OkAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            print("click the ok!")
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(OkAction)
+        self.present(alertController, animated: true, completion: nil)
+        
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
